ba2bd2e69eb9d3c2cd1ed341faf2fb0a0c374127
Ercenk Keresteci <ercenk@ercenk.com>
2015-04-14 01:25:43 -0700

    fixed couchbase sh

--- Couchbase/couchbase-azure-install.sh
+++ Couchbase/couchbase-azure-install.sh
@@ -1,1 +1,6 @@
- curl -X POST -H "content-type:text/plain" --data-binary "${HOSTNAME} - $1" https://logs-01.loggly.com/bulk/1ade465e-527c-40ab-a8b0-7c6f477af19a/tag/es-extension,${HOSTNAME}
+log()
+{
+    curl -X POST -H "content-type:text/plain" --data-binary "${HOSTNAME} - $1" https://logs-01.loggly.com/inputs/1ade465e-527c-40ab-a8b0-7c6f477af19a/tag/cb-extension,${HOSTNAME}
+}
+
+log "Begin execution of couchbase script extension on ${HOSTNAME}"
