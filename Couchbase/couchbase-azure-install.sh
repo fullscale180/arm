d3b8a20dd9e66a57c5be2998a8353539d4ebee4e
Ercenk Keresteci <ercenk@ercenk.com>
2015-04-14 02:01:42 -0700

    bash script

--- Couchbase/couchbase-azure-install.sh
+++ Couchbase/couchbase-azure-install.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
+
+### Ercenk Keresteci (Full Scale 180 Inc)
+### 
+### Warning! This script partitions and formats disk information be careful where you run it
+###          This script is currently under development and has only been tested on Ubuntu images in Azure
+###          This script is not currently idempotent and only works for provisioning
+
+# Log method to control/redirect log output
 log()
 {
     curl -X POST -H "content-type:text/plain" --data-binary "${HOSTNAME} - $1" https://logs-01.loggly.com/inputs/1ade465e-527c-40ab-a8b0-7c6f477af19a/tag/cb-extension,${HOSTNAME}
 }
 
-log "Begin couchbase script extension on ${HOSTNAME}"
+log "Begin execution of couchbase script extension on ${HOSTNAME}"
+ 
