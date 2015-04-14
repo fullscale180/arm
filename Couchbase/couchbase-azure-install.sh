266a2e68cca4f1d4d8409da3db1e4b0ccee9858c
Ercenk Keresteci <ercenk@ercenk.com>
2015-04-14 01:51:10 -0700

    couchbase sh file with unix line endigs

--- Couchbase/couchbase-azure-install.sh
+++ Couchbase/couchbase-azure-install.sh
@@ -3,4 +3,4 @@ log()
     curl -X POST -H "content-type:text/plain" --data-binary "${HOSTNAME} - $1" https://logs-01.loggly.com/inputs/1ade465e-527c-40ab-a8b0-7c6f477af19a/tag/cb-extension,${HOSTNAME}
 }
 
-log "Begin execution of couchbase script extension on ${HOSTNAME}"
+log "Begin couchbase script extension on ${HOSTNAME}"
