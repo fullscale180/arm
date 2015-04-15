e5d52c92faed763aa62330d6251ba4d48d1e4d52
Val M <killotonn@hotmail.com>
2015-04-14 19:39:26 -0700

    More changes for Nagios

--- nagios-on-ubuntu/nagios-core-install.sh
+++ nagios-on-ubuntu/nagios-core-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Script parameters and their defaults
+SERVICE_USERNAME="nagios"
 ADMIN_USERNAME="nagios"
 ADMIN_PASSWORD="Zk8LgLeR4ZimcgipTNzJKUBXVABDpYH63B9bzMbh2uRm8gYwRFPhSz8AvYspz3vs" # Don't worry, this is not the actual password. The real password will be supplied by the ARM template.
 CORE_VERSION="4.0.8"
@@ -14,6 +15,8 @@ help()
 	echo "This script installs and configures Nagios Core on the Ubuntu virtual machine image"
 	echo "Available parameters:"
 	echo "-u Admin_User_Name"
+	echo "-u Admin_User_Password"
+	echo "-v Core_Package_Version"
 }
 
 log()
@@ -33,7 +36,7 @@ then
 fi
 
 # Parse script parameters
-while getopts :u:p:h optname; do
+while getopts :u:p:v:h optname; do
   log "Option $optname set with value ${OPTARG}"
   
   case $optname in
@@ -43,6 +46,9 @@ while getopts :u:p:h optname; do
 	p) # Admin user name
 		ADMIN_PASSWORD=${OPTARG}
 		;;		
+	v) # Core package version
+		CORE_VERSION=${OPTARG}
+		;;	
     h)  # Helpful hints
 		help
 		exit 2
@@ -56,44 +62,55 @@ while getopts :u:p:h optname; do
 done
 
 # Install essentials
+log "Installing system essentials..."
 apt-get -y update
 
 #  Install Apache (a pre-requisite for Nagios)
+log "Installing Apache..."
 apt-get -y install apache2
 
 # Install MySQL (a pre-requisite for Nagios)
+log "Installing MySQL..."
 export DEBIAN_FRONTEND=noninteractive
 apt-get -q -y install mysql-server mysql-client
 mysqladmin -u root password $ADMIN_PASSWORD
 
 # Install PHP (a pre-requisite for Nagios)
+log "Installing PHP..."
 apt-get -y install php5 php5-mysql libapache2-mod-php5
 
 # Install LAMP prerequisites
+log "Installing other LAMP prerequisites..."
 apt-get -y install build-essential libgd2-xpm-dev apache2-utils
 
 # Restart apache2 service
+log "Restarting apache2 service..."
 service apache2 restart
 
-# Create a new nagios user account and give it a password
-useradd -m $ADMIN_USERNAME
-echo '${ADMIN_USERNAME}:${ADMIN_USERNAME}' | chpasswd
+# Create a new Nagios user account and give it a password
+log "Creating and configuring the Nagios service user account..."
+useradd -m $SERVICE_USERNAME
+echo '$SERVICE_USERNAME:$ADMIN_PASSWORD' | chpasswd -m
 
 # Create a new nagcmd group for allowing external commands to be submitted through the web interface. Add both the nagios user and the apache user to the group.
+log "Creating and configuring the Nagios security group for external access..."
 groupadd nagcmd
-usermod -a -G nagcmd $ADMIN_USERNAME
+usermod -a -G nagcmd $SERVICE_USERNAME
 usermod -a -G nagcmd www-data
 
 # Download Nagios and plugins
+log "Downloading Nagios package and plugins..."
 wget http://prdownloads.sourceforge.net/sourceforge/nagios/nagios-$CORE_VERSION.tar.gz
 wget http://nagios-plugins.org/download/nagios-plugins-$PLUGINS_VERSION.tar.gz
 
 # Install Nagios and plugins
+log "Configuring Nagios packages..."
 tar xzf nagios-$CORE_VERSION.tar.gz
 cd nagios-$CORE_VERSION
 ./configure --with-command-group=nagcmd
 
 # Compile and install nagios modules
+log "Compiling Nagios packages..."
 make all
 make install
 make install-init
@@ -101,19 +118,25 @@ make install-config
 make install-commandmode
 
 # Install Nagios Web interface
+log "Installing the Nagios Web interface..."
 /usr/bin/install -c -m 644 sample-config/httpd.conf /etc/apache2/sites-enabled/nagios.conf
 
 # Create a nagios admin account for logging into the Nagios web interface.
-htpasswd -c /usr/local/nagios/etc/htpasswd.users $ADMIN_USERNAME #ASKS FOR PWD!
+log "Creating the admin account for logging into the Nagios web interface..."
+htpasswd -cb /usr/local/nagios/etc/htpasswd.users $ADMIN_USERNAME $ADMIN_PASSWORD
 
 # Install Nagios plugins
+log "Configuring Nagios plugins..."
 tar xzf nagios-plugins-$PLUGINS_VERSION.tar.gz
 cd nagios-plugins-$PLUGINS_VERSION
 ./configure --with-nagios-user=nagios --with-nagios-group=nagios
+
+log "Compiling Nagios plugins..."
 make
 make install
 
 # Enable Apache’s rewrite and cgi modules
+log "Enabling Apache modules..."
 a2enmod rewrite
 a2enmod cgi
 service apache2 restart
@@ -122,6 +145,7 @@ service apache2 restart
 /usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg
 
 # Start nagios service and make it to start automatically on every boot
+log "Starting the Nagios service..."
 service nagios start
 ln -s /etc/init.d/nagios /etc/rcS.d/S99nagios
 
