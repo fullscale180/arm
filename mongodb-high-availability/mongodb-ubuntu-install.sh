df50ca7edcaf2895bbd289d851682858b5d0eb3d
Val M <killotonn@hotmail.com>
2015-04-17 21:27:45 -0700

    Updated script names

---
+++ mongodb-high-availability/mongodb-ubuntu-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#--------------------------------------------------------------------------------------------------
+# MongoDB Template for Azure Resource Manager (brought to you by Full Scale 180 Inc)
+#
+# This script installs MongoDB on each Azure virtual machine. The script will be supplied with
+# runtime parameters declared from within the corresponding ARM template.
+#--------------------------------------------------------------------------------------------------
+
