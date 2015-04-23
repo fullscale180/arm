2ec10babe5dce8108d382dd6e4dd196265786158
dennisan <dennisan@users.noreply.github.com>
2015-04-23 01:34:25 -0700

    Update jenkMstrInstall.sh

--- jenkMstrInstall.sh
+++ jenkMstrInstall.sh
@@ -12,12 +12,24 @@
 # This script has only been tested on Ubuntu 14.04 LTS and must be root
 ######################################################### 
 
+grep -q "${HOSTNAME}" /etc/hosts
+
+if [ $? == 0];
+then
+  echo "%{HOSTNAME} found in /etc/hosts"
+else
+  echo "${HOSTNAME} not found in  /etc/hosts"
+  sudo echo "127.0.0.1 ${HOSTNAME}" >> /etc/hosts
+  log "hostname %{HOSTNAME} added to /etc/hosts"
+fi
+
+
 #### Install Java
-apt-get -y install openjdk-7-jre
+apt-get -y update 
 apt-get -y install openjdk-7-jdk
 
 #### Install Jemkins
-wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-apt-get -y update 
-echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list
+wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt-get update
 apt-get -y install jenkins
